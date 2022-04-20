d70c0b4338cbe58ad1a8544d51ff08b1c4269d55
KADAMBARI RAMESH PUNDE <104066262+Kadambaripunde@users.noreply.github.com>
2022-04-20 06:00:46 -0400

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -8,4 +8,4 @@ streamlit.header('🥋 - Shirt,📓 - Book')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(My_Fruit_List)
+streamlit.dataframe(my_fruit_list)
