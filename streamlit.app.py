0b0f9dd693e0a7e8fc971941b35adb4c62f181a3
KADAMBARI RAMESH PUNDE <104066262+Kadambaripunde@users.noreply.github.com>
2022-04-20 05:53:12 -0400

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title('My parents new healthy dinners')
 streamlit.title('My moms new healthy dinners')
+streamlit.header('Today menu')
+streamlit.text('breakfast, dinner & lunch')
