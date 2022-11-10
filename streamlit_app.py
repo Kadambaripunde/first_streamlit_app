c4d10b7af12870c6b8f27aa9615ad89821904e20
KADAMBARI PUNDE <104066262+Kadambaripunde@users.noreply.github.com>
2022-11-10 08:53:48 -0500

    Update streamlit_app.py
    Adding new statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 and Blueberry Oatmeal')
+streamlit.title('Kate, Spinach & Rocket smoothie')
+streamlit.title('Hard-boiled Free-Range egg')
